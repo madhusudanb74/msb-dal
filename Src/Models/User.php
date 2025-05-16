95d0f7e7b6e8f246263acb3b3db56a904c29b822
Madhusudan <madhusudanb74@gmail.com>
2025-05-16 23:53:43 +0530

    fist

---
+++ Src/Models/User.php
@@ -0,0 +1,21 @@
+<?php
+namespace DAL\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class User extends Model {
+
+    static function getName(){
+
+        return "Madhusudan";
+
+    }
+
+     static function getEmail(){
+
+        return "Madhusudan";
+
+    }
+
+}
